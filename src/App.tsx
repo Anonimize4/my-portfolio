a03076a29524da7fafe001c323174ae0f00cd236
Anonism123 <samuelbelay81@gmail.com>
2025-11-26 07:27:11 -0500

    stayles add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Header from './components/Header'
 import Home from './pages/home'
+import About from './pages/about'
+import Skills from './pages/skills'
+import Projects from './pages/projects'
+import Contact from './pages/contact'
+import Certificates from './pages/certificates'
 
 function App() {
-  return <Home />
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Certificates />
+      </main>
+    </div>
+  )
 }
 
 export default App
